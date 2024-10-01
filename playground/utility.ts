5da3bc7ce9641c457ec697e37f86c75f63f2fc99
lebrancconvas <lebrancconvas@gmail.com>
2024-10-01 23:35:08 +0700

    ✨ feat: update utility type code

--- playground/utility.ts
+++ playground/utility.ts
@@ -46,3 +46,63 @@ type CustomPick<T, K extends keyof T> = {
     [P in K]: T[P];
 };
 
+// Omit<Type, Keys>: Making an object type by picking all properties from Type and then removing Keys.
+type ExampleOmit = Omit<{title: string, view: number}, 'view'>;
+
+type CustomOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+// Exclude<UnionType, ExcludedMembers>: Exclude from UnionType those types that are assignable to ExcludedMembers.
+type ExampleExclude = Exclude<'title' | 'view' | 'comment', 'view' | 'comment'>;
+
+type CustomExclude<T, U> = T extends U ? never : T;
+
+// Extract<Type, Union>: Extract from Union those types that are assignable to Type.
+type ExampleExtract = Extract<'Title' | 'View', 'View' | 'Comment'>;
+
+type CustomExtract<T, U> = T extends U ? T : never;
+
+// NonNullable<Type>: Exclude null and undefined from Type.
+type ExampleNonNullable = NonNullable<string | number | undefined | null | [null] | [string, number]>;
+
+type CustomNonNullable<T> = T & {};
+
+// Parameters<Type>: Obtain the parameters of a function type in a tuple.
+type ExampleParameters = Parameters<(user: string) => {name: string, level: number}>;
+
+type CustomParameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+
+// ConstructorParameters<Type>: Obtain the parameters of a constructor function type in a tuple.
+type ExampleConstructorParameters = ConstructorParameters<ErrorConstructor>;
+
+type CustomConstructorParameters<T extends abstract new (...args: any) => any> = T extends abstract new (...args: infer P) => any ? P : never;
+
+// ReturnType<Type>: Obtain the return type of a function type.
+type ExampleReturnType = ReturnType<(user: string) => {name: string, level: number}>;
+
+type CustomReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+
+// InstanceType<Type>: Obtain the instance type of a constructor function type.
+type ExampleInstanceType = InstanceType<RegExpConstructor>;
+
+type CustomInstanceType<T extends abstract new (...args: any) => any> = T extends abstract new (...args: any) => infer R ? R : any;
+
+// NoInfer<Type>: Prevents inference of Type.
+function createAngle<T extends number>(angles: T[], selectedAngle: NoInfer<T>) {console.log(selectedAngle)};
+createAngle([30, 60, 90], 90);
+
+type CustomNoInfer<T> = intrinsic;
+
+// ThisParameterType<Type>: Obtain the `this` parameter type of a function type.
+type ExampleThisParameterType = ThisParameterType<(this: string, haveSword: boolean) => {name: string, level: number}>;
+
+type CustomThisParameterType<T> = T extends (this: infer U, ...args: never) => any ? U : unknown;
+
+// OmitThisParameter<Type>: Remove the `this` parameter in a function type.
+type ExampleOmitThisParameter = OmitThisParameter<(this: string, haveSword: boolean) => {name: string, level: number}>;
+
+type CustomOmitThisParameter<T> = unknown extends ThisParameterType<T> ? T : T extends (...args: infer A) => infer R ? (...args: A) => R : T;
+
+// ThisType<Type>: ThisType<Type> is a marker for contextual this types. (Need more clarifying).
+type ExampleThisType = ThisType<{name: string} & {level: number}>;
+
+interface CustomThisType<T> {};
